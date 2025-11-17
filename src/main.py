992e8890405604052434fb0b421c9b9d712da0e6
Jnx Jnx <SCoVader@proton.me>
2025-11-17 19:14:37 +0200

    Add extraction of images and links from strings

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
-from textnode import TextNode, TextType, text_to_html_node
-from htmlnode import HTMLNode, LeafNode, ParentNode
-from inline_markdown import split_nodes_delimiter
+# from textnode import TextNode, TextType, text_to_html_node
+# from htmlnode import HTMLNode, LeafNode, ParentNode
+from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 
 
 def main():
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 from textnode import TextNode, TextType
-from inline_markdown import split_nodes_delimiter
+from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 
 
 class TestInlineMarkdown(unittest.TestCase):
@@ -83,6 +83,13 @@ class TestInlineMarkdown(unittest.TestCase):
             new_nodes,
         )
 
+    def test_image_extraction(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        self.assertEqual(extract_markdown_images(text), [('rick roll', 'https://i.imgur.com/aKaOqIh.gif'), ('obi wan', 'https://i.imgur.com/fJRm4Vk.jpeg')])
+
+    def test_link_extraction(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        self.assertEqual(extract_markdown_links(text), [('to boot dev', 'https://www.boot.dev'), ('to youtube', 'https://www.youtube.com/@bootdotdev')])
 
 if __name__ == "__main__":
     unittest.main()
