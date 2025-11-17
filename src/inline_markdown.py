9f98777c662e8d13dc4b1d564b8d16455b55c72b
Jnx Jnx <SCoVader@proton.me>
2025-11-17 21:56:39 +0200

    Added node splitters for images and links

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -22,10 +22,61 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         new_nodes.extend(split_nodes)
     return new_nodes
 
+
 def extract_markdown_images(text):
     return re.findall(r"!\[([^\[\]]*)\]\(([^\(\) ]*)\)", text)
     #return re.findall(r"!\[([\w ]*)\]\((\S*.\w*)\)", text)
 
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        delim_tuples = extract_markdown_images(old_node.text)
+        split_nodes = []
+        new_string = old_node.text
+        substrings = []
+        for i in range(len(delim_tuples)):
+            delim = f"![{delim_tuples[i][0]}]({delim_tuples[i][1]})"
+            substrings = new_string.split(delim, 1)
+            if len(substrings) < 1:
+                continue
+            if substrings[0] != '': 
+                split_nodes.append(TextNode(substrings[0], TextType.TEXT))
+            split_nodes.append(TextNode(delim_tuples[i][0], TextType.IMAGE, delim_tuples[i][1]))
+            if substrings[1] != '': 
+                new_string = substrings[1]
+        if len(substrings) > 1 and substrings[1] != '': 
+            split_nodes.append(TextNode(substrings[1], TextType.TEXT))
+        new_nodes.extend(split_nodes)
+    return new_nodes
+
 def extract_markdown_links(text):
     return re.findall(r"\[([^\[\]]*)\]\(([^\(\) ]*)\)", text)
     #return re.findall(r"\[([\w ]*)\]\((\S*)\)", text)
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        delim_tuples = extract_markdown_links(old_node.text)
+        split_nodes = []
+        new_string = old_node.text
+        substrings = []
+        for i in range(len(delim_tuples)):
+            delim = f"[{delim_tuples[i][0]}]({delim_tuples[i][1]})"
+            substrings = new_string.split(delim, 1)
+            if len(substrings) < 1:
+                continue
+            if substrings[0] != '': 
+                split_nodes.append(TextNode(substrings[0], TextType.TEXT))
+            split_nodes.append(TextNode(delim_tuples[i][0], TextType.LINK, delim_tuples[i][1]))
+            if substrings[1] != '': 
+                new_string = substrings[1]
+        if len(substrings) > 1 and substrings[1] != '': 
+            split_nodes.append(TextNode(substrings[1], TextType.TEXT))
+        new_nodes.extend(split_nodes)
+    return new_nodes
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,12 @@
 import unittest
 from textnode import TextNode, TextType
-from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
+from inline_markdown import (
+    split_nodes_delimiter, 
+    extract_markdown_images, 
+    extract_markdown_links,
+    split_nodes_image,
+    split_nodes_link
+)
 
 
 class TestInlineMarkdown(unittest.TestCase):
@@ -85,11 +91,63 @@ class TestInlineMarkdown(unittest.TestCase):
 
     def test_image_extraction(self):
         text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
-        self.assertEqual(extract_markdown_images(text), [('rick roll', 'https://i.imgur.com/aKaOqIh.gif'), ('obi wan', 'https://i.imgur.com/fJRm4Vk.jpeg')])
+        self.assertEqual(extract_markdown_images(text), 
+            [
+                ('rick roll', 'https://i.imgur.com/aKaOqIh.gif'), 
+                ('obi wan', 'https://i.imgur.com/fJRm4Vk.jpeg')
+            ]
+        )
 
     def test_link_extraction(self):
         text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
-        self.assertEqual(extract_markdown_links(text), [('to boot dev', 'https://www.boot.dev'), ('to youtube', 'https://www.youtube.com/@bootdotdev')])
+        self.assertEqual(extract_markdown_links(text), 
+            [
+                ('to boot dev', 'https://www.boot.dev'), 
+                ('to youtube', 'https://www.youtube.com/@bootdotdev')
+            ]
+        )
+
+    def test_split_images_complex(self):
+        node = TextNode("This is huge text node with multiple images ![first image](https://example.com/image1.png), some text between them ![second image](https://example.com/image2.jpg), some more text ![third image](https://example.com/image3.gif) and final few words", TextType.TEXT)
+        expected = [
+            TextNode('This is huge text node with multiple images ', TextType.TEXT),
+            TextNode('first image', TextType.IMAGE, 'https://example.com/image1.png'),
+            TextNode(', some text between them ', TextType.TEXT),
+            TextNode('second image', TextType.IMAGE, 'https://example.com/image2.jpg'),
+            TextNode(', some more text ', TextType.TEXT),
+            TextNode('third image', TextType.IMAGE, 'https://example.com/image3.gif'),
+            TextNode(' and final few words', TextType.TEXT)
+        ]
+        self.assertListEqual(split_nodes_image([node]), expected)
+
+    def test_split_images_no_dangling_nodes(self):
+        node = TextNode("This is huge text node with multiple images ![first image](https://example.com/image1.png)", TextType.TEXT)
+        expected = [
+            TextNode('This is huge text node with multiple images ', TextType.TEXT),
+            TextNode('first image', TextType.IMAGE, 'https://example.com/image1.png'),
+        ]
+        self.assertListEqual(split_nodes_image([node]), expected)
+
+    def test_split_images_no_dangling_nodes(self):
+        node = TextNode("![first image](https://example.com/image1.png), some text between them ", TextType.TEXT)
+        expected = [
+            TextNode('first image', TextType.IMAGE, 'https://example.com/image1.png'),
+            TextNode(', some text between them ', TextType.TEXT),
+        ]
+        self.assertListEqual(split_nodes_image([node]), expected)
+
+    def test_split_links_complex(self):
+        node = TextNode("This is huge text node with multiple links [first image](https://example.com/image1.png), some text between them [second image](https://example.com/image2.jpg), some more text [third image](https://example.com/image3.gif) and final few words", TextType.TEXT)
+        expected = [
+            TextNode('This is huge text node with multiple links ', TextType.TEXT),
+            TextNode('first image', TextType.LINK, 'https://example.com/image1.png'),
+            TextNode(', some text between them ', TextType.TEXT),
+            TextNode('second image', TextType.LINK, 'https://example.com/image2.jpg'),
+            TextNode(', some more text ', TextType.TEXT),
+            TextNode('third image', TextType.LINK, 'https://example.com/image3.gif'),
+            TextNode(' and final few words', TextType.TEXT)
+        ]
+        self.assertListEqual(split_nodes_link([node]), expected)
 
 if __name__ == "__main__":
     unittest.main()
