6ff7abe162343986f9f856e580c0cd5843b73e95
Jnx Jnx <SCoVader@proton.me>
2025-11-20 20:43:35 +0200

    more content + recursive page generation

--- src/main.py
+++ src/main.py
@@ -38,6 +38,7 @@ def cleanup_n_copy_content(src='./static/', dst = './public/'):
     copy_tree(src, dst)
     os.listdir(dst)
 
+
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path) as markdown_file:
@@ -54,12 +55,27 @@ def generate_page(from_path, template_path, dest_path):
     with open(dest_path, 'x') as output:
         output.write(full)
 
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    if not os.path.exists(dir_path_content):
+        raise Exception("Invalid content folder")
+    items = os.listdir(dir_path_content)
+    for item in items:
+        src = os.path.join(dir_path_content, item)
+        dst = os.path.join(dest_dir_path, item)
+        if os.path.isdir(src):
+            if not os.path.exists(dst):
+                os.mkdir(dst)
+            generate_pages_recursive(src, template_path, dst)
+        if os.path.isfile(src):
+            dst = f'{dst[0:-3]}.html'
+            generate_page(src, template_path, dst)
+
 def main():
-    source = "./content/index.md"
-    destination = "./public/index.html"
+    source = "./content/"
+    destination = "./public/"
     template = "./template.html"
     cleanup_n_copy_content()
-    generate_page(source, template, destination)
+    generate_pages_recursive(source, template, destination)
     
 
 if __name__ == "__main__":
